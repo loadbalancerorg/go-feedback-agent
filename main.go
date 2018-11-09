39f624e022840641032d241a4de516826c5ecfdc
andrew-loadbalancer <andrew@loadbalancer.org>
2018-11-09 11:18:28 +0000

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ var logger service.Logger
 type program struct{}
 
 func (p *program) Start(s service.Service) error {
-	// Start should not block. Do the actual work async.
 	go p.run()
 	return nil
 }
@@ -18,9 +17,7 @@ func (p *program) run() {
 	// Do work here
 	InitConfig()
 	srv := InitServer()
-	// accept connection on port
 
-	// run loop forever (or until ctrl-c)
 	for {
 		conn, err := srv.server.Accept()
 		if err != nil {
@@ -31,7 +28,6 @@ func (p *program) run() {
 }
 
 func (p *program) Stop(s service.Service) error {
-	// Stop should not block. Return with a few seconds.
 	return nil
 }
 
@@ -55,5 +51,3 @@ func main() {
 		logger.Error(err)
 	}
 }
-
-//http://decouvric.cluster013.ovh.net/golang/thirdparty/divers/creer-un-service-golang-avec-kardianos.html
